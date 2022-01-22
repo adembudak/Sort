f07334bb6d0306896a53d92212c30ee5711910a6
vanilla milkshake <vanillamilkshakepls@gmail.com>
2022-01-22 14:02:55 +0300

    Use std:invoke to call comparison predicate

--- include/sort/insertion.hpp
+++ include/sort/insertion.hpp
@@ -25,19 +25,17 @@ constexpr T insertion(T first, T last, Compare comp = {}) {
 namespace ranges {
 
 struct insertion_fn {
-  template <std::random_access_iterator I, std::sentinel_for<I> S,
-            typename Comp = std::ranges::less, typename Proj = std::identity>
-  requires std::sortable<I, Comp, Proj>
+  template <std::random_access_iterator I, std::sentinel_for<I> S, typename Comp = std::ranges::less, typename Proj = std::identity>
+    requires std::sortable<I, Comp, Proj>
   constexpr I operator()(I first, S last, Comp comp = {}, Proj proj = {}) const {
     if (first == last)
       return first;
 
     std::size_t N = std::ranges::distance(first, last);
-
     I last_iter = std::ranges::next(first, last);
 
     for (std::size_t i = 1; i < N; ++i) {
-      for (std::size_t j = i; j > 0 && comp(*(first + j), *(first + j - 1)); --j) {
+      for (std::size_t j = i; j > 0 && std::invoke(comp, invoke(proj, *(first + j)), invoke(proj, *(first + j - 1))); --j) {
         std::iter_swap(first + j, first + j - 1);
       }
     }
@@ -45,11 +43,9 @@ struct insertion_fn {
     return last_iter;
   }
 
-  template <std::ranges::random_access_range R, class Comp = std::ranges::less,
-            class Proj = std::identity>
-  requires std::sortable<std::ranges::iterator_t<R>, Comp, Proj>
-  constexpr std::ranges::borrowed_iterator_t<R> operator()(R &&r, Comp comp = {},
-                                                           Proj proj = {}) const {
+  template <std::ranges::random_access_range R, class Comp = std::ranges::less, class Proj = std::identity>
+    requires std::sortable<std::ranges::iterator_t<R>, Comp, Proj>
+  constexpr std::ranges::borrowed_iterator_t<R> operator()(R &&r, Comp comp = {}, Proj proj = {}) const {
     return (*this)(std::ranges::begin(r), std::ranges::end(r), std::move(comp), std::move(proj));
   }
 };
